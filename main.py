c5e8618371017794219c705a0298e28fb7955d2d
zack149 <zpjordan18@gmail.com>
2021-08-26 11:17:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def sum_of_squares(a):
-	pass
-
+    result = 0
+    for i in a:
+        result += i*i
+    return result
+    
 def test_one():
     assert sum_of_squares([1,2,3]) == 14
+    
+def test_two():
+    assert (True == 1)
+
+test_one()
+test_two()
